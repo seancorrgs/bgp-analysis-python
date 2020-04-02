7316d931da0309a1e3835608be22a6aac5c7a23d
imp4ct <sean.corrigan@platinumnetworks.ca>
2020-04-02 17:36:43 -0400

    Added features to change edges

--- Main.py
+++ Main.py
@@ -1,24 +1,116 @@
-def matrixexists(s4arch, matrix):
+##########################
+# Sean Corrigan 2020
+# 100693058
+# https://gitlab.platinumnetworks.ca/imp4ct
+##########################
+
+def matrixexists(s4arch, matrix): ## Check if the city exists in the matrix
     if s4arch in matrix[0]:
         return True
 
-
-## Find the index of a location in the matrix
-def matrixcityloc(s4arch, matrix):
+def matrixcityloc(s4arch, matrix): ## Find the index of a location in the matrix
     loc = matrix[0].index(s4arch)
-    return loc ## Return the x/y of where that city is
+    return loc # Return the x/y of where that city is
 
-def returnedgeweight(city1, city2, matrix):
+def returnedgeweight(city1, city2, matrix): ## Return the edge weight of 2 cities
     city1loc = matrixcityloc(city1, matrix); city2loc = matrixcityloc(city2, matrix)
-    return matrix[city2loc][city1loc]
-    # Need to search for the location of city 1 and city 2 first the    
+    weight =  matrix[city2loc][city1loc]
+    try:
+        weight = int(weight)
+    except: return False
+    return weight
 
-def displaymatrix(matrix):
-    for x in matrix:
-        print(x)
+def displaymatrix(matrix): ## Print the matrix nicely for the debug screen
+    s = [[str(e) for e in row] for row in matrix]
+    lens = [max(map(len, col)) for col in zip(*s)]
+    fmt = '\t'.join('{{:{}}}'.format(x) for x in lens)
+    table = [fmt.format(*row) for row in s]
+    print('\n'.join(table))
 
+def listcities(matrix): ## Return a list of the cities in the array
+    citylist = []
+    for i in range(len(matrix[0])):
+        if i == 0: continue # Dont Index Placeholder
+        citylist.append(matrix[0][i])
+    return citylist
+
+# def returnpaths(city1, matrix): ## return a list of the paths len available to a city
+#     loc = matrixcityloc(city1, matrix)
+#     return(matrix[loc])
+
+def returnpaths(city1, matrix): ## Return a dictionary of each path and weight from a point
+    loc = matrixcityloc(city1, matrix) # find the xy of city
+    cityweight = matrix[loc] # pick the weights froms the matrix
+    cityweight.pop(0) # RM name of the path
+    pathdict = dict() # init Dictionary
+    for i in range(len(cityweight)):
+        # if cityweight[i] == "------": continue we need to keep the structure of the matrix to be able to convert back
+        pathdict[matrix[0][i+1]] = cityweight[i]
+    return pathdict
+    ### Ref
+    # Provice city name and the matrix 
+    # Paths will be returned as a DICT
+
+def changeedge():
+    global matrix # We want to edit matricies easier + error handeling
+    availcities = listcities(matrix)
+    print("-- Please choose starting node --")
+    for i in range(len(availcities)):
+        print("{}. {}".format(i, availcities[i]))
+    try:
+        choicecity1 = int(input("choice:   "))
+    except ValueError: print("You must enter a int value"); return
+    
+    ## Choose a city to view the edges on it
+    availpaths = returnpaths(availcities[choicecity1], matrix) # Get all paths for the chosen city
+    print("-- Current Paths --\n\n"); counter = 0; # , start counter
+    for k, v in availpaths.items(): # display all dictionary keys
+        print("{}. {} - {}km".format(counter,k,v)); counter += 1
+    try: # Ask for user to choose a path to modify
+        choice = int(input("Path to modify (hit enter to return):   "))
+    except ValueError: input("\n\n\nYou must enter a int value,\npress enter to return to main menu"); return
+    
+    ## convert the Path into a city name
+    counter = 0
+    for k, v in availpaths.items():
+        if counter == choice: choicecity2 = k
+        counter += 1 
+    
+    ## set new value in matrix
+    try: # Ask for user for new weight
+        
+        newfloat = float(input("New weight (hit enter to cancel modification):   "))
+    except ValueError: input("\n\n\nYou must enter a int value,\npress enter to return to main menu"); return
+    availpaths[choicecity2] = newfloat # Update dict with new value
+    pushmatrix = [listcities(matrix)[choicecity1]] # matrix to add back [add name into matrix]
+    for k, v in availpaths.items(): # add all values back into a matrix
+        pushmatrix.append(v)
+    matrix[choicecity1 +1] = pushmatrix # push to main matrix
+    ### ONLY CHANGES THE FROM MATRIX FOR THAT NODE ###
+        # need to change it in the other axis for #
+
+def deleteedge():
+    global matrix # We want to edit matricies easier + error handeling
+        availcities = listcities(matrix)
+        print("-- Please choose starting node --")
+        for i in range(len(availcities)):
+            print("{}. {}".format(i, availcities[i]))
+        try:
+            choicecity1 = int(input("choice:   "))
+        except ValueError: print("You must enter a int value"); return
+    
+    ## Choose a city to view the edges on it
+    availpaths = returnpaths(availcities[choicecity1], matrix) # Get all paths for the chosen city
+    print("-- Current Paths --\n\n"); counter = 0; # , start counter
+    for k, v in availpaths.items(): # display all dictionary keys
+        print("{}. {} - {}km".format(counter,k,v)); counter += 1
+    try: # Ask for user to choose a path to modify
+        choice = int(input("Path to modify (hit enter to return):   "))
+    except ValueError: input("\n\n\nYou must enter a int value,\npress enter to return to main menu"); return   
+        
 def importdata():
-    matrix = [["_______"]]
+    global matrix
+    matrix = [["------"]]
     with open("network.txt","r") as file:
         while True:
             line = file.readline()
@@ -49,34 +141,46 @@ def importdata():
                     matrix[city1].append("------")
     return matrix
 
+def saveall(matrix): # Save the updated matrix
+    print("Forcing Save")
+    print("Save Complete.")
+
+
 
 def debug(matrix):
      # Testing Area
     print("\nTESTING\n-------")
     displaymatrix(matrix)
-    print("\nOttawa to Oshawa Dist.")
-    print(returnedgeweight("Ottawa","Oshawa",matrix)) 
-    # loc = matrixcityloc("", matrix)
-    # print(loc)
-    # print(matrix[loc][0], matrix[0][loc])
-    input("")
+        # print("\nOttawa to Oshawa Dist.")
+        # print(returnedgeweight("Ottawa","Kingston", matrix)) 
+    print(listcities(matrix))
+        # dict = returnpaths("Oshawa", matrix)
+        # print(dict)
+        # # loc = matrixcityloc("", matrix)
+        # # print(loc)
+        # # print(matrix[loc][0], matrix[0][loc])
+        # input("")
+
 
 def init():
     while True:
         print("\n" * 50)
         print("Choose a function\n\
-            1. Build initial Graph\n\
+            1. Build initial Graph from \"network.txt\"\n\
             2. Change an Edge weight\n\
             3. Remove Edge\n\
             4. Find and Display all shortest paths\n\
             5. Minimum Spanning Tree\n\
-            6. Quit\n\
+            6. Save-all\n\
+            7. Quit\n\
             9. Debug")
-        choice = input("choice:   ")
+        try:
+            choice = int(input("choice:   "))
+        except ValueError: print("You must enter a int value")
         if int(choice) == 1:
-            importdata()
+            matrix = importdata()
         elif int(choice) == 2:
-            print(choice)
+            changeedge()
         elif int(choice) == 3:
             print(choice)
         elif int(choice) == 4:
@@ -84,11 +188,13 @@ def init():
         elif int(choice) == 5:
             print(choice)
         elif int(choice) == 6:
+            save-all()
+        elif int(choice) == 7:
             break
         elif int(choice) == 9:
             debug(matrix)
-        
-        matrix = importdata()
+        else: continue
+       
        
 
 if __name__ == "__main__":
